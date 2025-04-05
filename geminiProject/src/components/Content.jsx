e2ddfcdc44ba5e7dd4c9c03493af4bc7ee91914e
Tani-2007 <saxenatanishq4567@gmail.com>
2025-04-06 03:29:07 +0530

    first

--- geminiProject/src/components/Content.jsx
+++ geminiProject/src/components/Content.jsx
@@ -4,7 +4,7 @@ export default function Content({ data, loading }) {
   if (loading) {
     return (
       <div className=" bg-gray-300 flex  items-center justify-center h-110">
-        <h1 className="font-bold text-5xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+        <h1 className="font-bold text-5xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text text-center">
           What Up New User ?
         </h1>
       </div>
--- geminiProject/src/components/Header.jsx
+++ geminiProject/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       className="flex
      justify-between p-2 content-center"
     >
-      <h1 className="text-2xl font-bold px-3 py-2">Gemini</h1>
+      <h1 className="text-2xl font-bold px-3 py-2">Nova</h1>
       <img
         className="h-8 mt-2 px-2 "
         src="https://cdn-icons-png.freepik.com/256/1077/1077114.png?semt=ais_hybrid"
